a6a786230dc84c3d851395d5c67451696d03134c
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-15 23:09:36 -0500

    este es el main de la aplicacion de consola, solo instancia a la clase Principal

---
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,24 @@
+package com.alura.literalura;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.alura.literalura.repositories"})
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private Principal principal;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		principal.startLoop();
+	}
+}
